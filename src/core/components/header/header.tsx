93e6bc29fede908342e81556e3e7346d8a7a9f15
Jesus <jesus.cassano.2006@gmail.com>
2023-02-08 00:59:12 +0100

    tsx files modify

--- src/core/components/header/header.tsx
+++ src/core/components/header/header.tsx
@@ -1,23 +1,9 @@
-import logo from "./logo.svg";
+import "./header.css";
 
-type HeaderProps = { children: JSX.Element };
-
-export function Header({ children }: HeaderProps) {
+export function Header() {
   return (
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-      {children}
+    <header>
+      <h1>Formulario</h1>
     </header>
   );
 }
